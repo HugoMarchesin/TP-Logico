76a714536861d659fded7f6f3ab802d0974beca9
Hugo <hugo.elio.marchesin@gmaile.com>
2024-09-08 17:02:29 -0300

    Hice hasta el 3. El 4 está mal

--- TP.pl
+++ TP.pl
@@ -11,7 +11,27 @@ persona(pepe, boca(chica)).
 persona(pepe, cara(puntuda)).
 persona(pepe, ojos(marrones)).
 persona(pepe, nariz(chica)).
+
+%punto2
+persona(Persona, lentes(marrones)) :- persona(Persona, nariz(chica)), persona(Persona, boca(grande)).
+persona(Persona, ojos(marrones)) :- persona(Persona, pelo(morocho)).
+persona(Persona, ojos(marrones)) :- persona(Persona, pelo(castanio)).
+persona(Persona, cara(redonda)) :- not(persona(Persona, cara(puntuda))).
+
 carta(rojo, samuel).
 carta(azul, pepe).
 contrincante(rojo, azul).
 contrincante(azul, rojo).
+
+%punto1
+esCalvo(Persona) :- persona(Persona, calvo).
+esRubio(Persona) :- persona(Persona, pelo(rubio,_)).
+tieneLentes(Persona) :- persona(Persona, lentes(_)).
+
+%punto3
+esRubioYBocaChica(Persona) :- esRubio(Persona), persona(Persona, boca(chica)).
+
+%punto4
+pista(rojo,pelo(rubio,_)).
+pista(rojo,boca(chica)).
+coincide(Persona) :- persona(Persona, Caracteristica), pista(_, Caracteristica), not((persona(Persona, Caracteristica2), not(pista(_, Caracteristica2)))).
